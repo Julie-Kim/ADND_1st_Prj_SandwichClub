2611edd3fb0e6b07b13a2b443eae4c8bcebf0119
Julie <juliekim8312@gmail.com>
2020-01-30 23:05:50 +0900

    [Review suggestion] Try to use the safe **optString()** method instead of getString() which will return an empty string instead of null if the value for the that key is not found in the JSON Object.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -32,14 +32,14 @@ public class JsonUtils {
             JSONObject sandwichObject = new JSONObject(json);
 
             JSONObject nameObject = sandwichObject.getJSONObject(KEY_NAME);
-            String mainName = nameObject.getString(KEY_MAIN_NAME);
+            String mainName = nameObject.optString(KEY_MAIN_NAME);
             List<String> alsoKnownAs = getStringListFromJsonArray(nameObject.getJSONArray(KEY_ALSO_KNOWN_AS));
 
-            String placeOfOrigin = sandwichObject.getString(KEY_PLACE_OF_ORIGIN);
+            String placeOfOrigin = sandwichObject.optString(KEY_PLACE_OF_ORIGIN);
 
-            String description = sandwichObject.getString(KEY_DESCRIPTION);
+            String description = sandwichObject.optString(KEY_DESCRIPTION);
 
-            String image = sandwichObject.getString(KEY_IMAGE);
+            String image = sandwichObject.optString(KEY_IMAGE);
 
             List<String> ingredients = getStringListFromJsonArray(sandwichObject.getJSONArray(KEY_INGREDIENTS));
 
@@ -53,10 +53,10 @@ public class JsonUtils {
         }
     }
 
-    private static List<String> getStringListFromJsonArray(JSONArray jsonArray) throws JSONException {
+    private static List<String> getStringListFromJsonArray(JSONArray jsonArray) {
         List<String> stringList = new ArrayList<>();
         for (int i = 0; i < jsonArray.length(); i++) {
-            stringList.add(jsonArray.getString(i));
+            stringList.add(jsonArray.optString(i));
         }
         return stringList;
     }
